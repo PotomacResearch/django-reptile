2f098045f399efd93d6dac89506c06dd61b5ec65
Colin McCann <colin@potomacresear.ch>
2023-05-30 15:16:11 -0400

    Simple login workflow

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-LOGIN_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "home"
 
 AUTH_PASSWORD_VALIDATORS = [
