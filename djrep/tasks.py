50bae305be5e2c9d09253419a1c31ac56739893a
Colin McCann <colin@potomacresear.ch>
2023-06-08 12:22:15 -0400

    Tweaks for Ubuntu

--- djrep/tasks.py
+++ djrep/tasks.py
@@ -1,7 +1,5 @@
 from huey.contrib.djhuey import task
 
-from reptile import toysystems
-
 from djrep.task_helpers import start_training,complete_training
 
 
@@ -9,6 +7,9 @@ from djrep.task_helpers import start_training,complete_training
 def run_dummy_task(reptile_training_id):
     task_name = start_training(reptile_training_id)
 
+    # Import here so that Django doesn't require running reptile to serve
+    #    the website
+    from reptile import toysystems
     library = toysystems.sine_example()
     print(f'{task_name} completed training sine: {library}')
 
